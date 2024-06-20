f918925166b4cec16c36ebba15e4ef8739d12e0b
Saulo Andrade <sauloandrade@gmail.com>
2024-06-20 02:07:47 -0300

    adding first version of prediction method

--- src/fsp/fsp.py
+++ src/fsp/fsp.py
@@ -1,15 +1,138 @@
 from sklearn.cluster import KMeans
 from fsp.divergence_measure.scipy import DivergenceMeasureScipy
 from fsp.options import Options
+from scipy.spatial import distance
 import numpy as np
 import time
 
 class Fsp:
 
-    def __init__(self, X = [], y = [],opt = Options()):
-        self.X = X
-        self.y = y
+    def __init__(self, opt = Options()):
+        self.X = []
+        self.y = []
         self.opt = opt
+
+    def predict_case1(self, Xtest,rmH,Xtrain,ytrain):
+        # Step 0: Retrieve the cluster centroid removed by the FSP method
+        C = np.vstack(rmH['rmC']) # k x d matrix of cluster centroids
+
+        # Step 1: Classify the Xtrain dataset by finding the nearest centroid for each observation
+        dist_Xtrain_C = distance.cdist(Xtrain, C, 'euclidean')  # Ntrain x k matrix of distances between Xtrain observations and centroids
+        Itrain = np.argmin(dist_Xtrain_C, axis=1)  # Array of size Ntrain, index of the nearest centroid for each observation in Xtrain
+
+        # Step 2: Calculate the dominant class label and its proportion for each cluster in the training dataset
+        k = C.shape[0]  # Number of clusters
+        n = np.unique(ytrain).size  # Number of unique classes
+        count_matrix = np.zeros((k, n), dtype=int)  # Initialize a k x n matrix to count occurrences of each class in each cluster
+        np.add.at(count_matrix, (Itrain, ytrain), 1)  # Count the occurrences of each class in each cluster
+
+        DominantClassLabel = np.argmax(count_matrix, axis=1)  # Dominant class label for each cluster
+        DominantClassLabel_Proportion = count_matrix[np.arange(k), DominantClassLabel] / np.sum(count_matrix, axis=1)  # Proportion of the dominant class in each cluster
+        #DominantClassLabel_Proportion = np.max(count_matrix, axis=1) / np.sum(count_matrix, axis=1)
+
+        # Step 3: Classify the Xtest dataset by finding the nearest centroid for each observation
+        dist_Xtest_C = distance.cdist(Xtest, C, 'euclidean')  # Ntest x k matrix of distances between Xtest points and centroids
+        Itest = np.argmin(dist_Xtest_C, axis=1)  # Array of size Ntest, index of the nearest centroid for each observation in Xtest
+
+        # Step 4: Assign the predicted class label and its proportion for each observation in Xtest
+        ypredict = DominantClassLabel[Itest] # Predicted class labels for Xtest
+        ypredict_Proportion = DominantClassLabel_Proportion[Itest] # Proportion of the predicted class labels for Xtest
+
+        return ypredict, ypredict_Proportion
+
+
+    def check_homogeneity(self, y, idx, k:int, nCL:int, p_parameter:float, h_threshold:int) -> tuple:
+        """
+        Evaluate the homogeneity of clusters based on class label distribution within each cluster.
+
+        Parameters:
+        y (numpy.ndarray): m-element vector where each entry contains the label of the corresponding observation. Labels are integers between 0 and nCL-1 (nCL is the number of unique class labels).
+        idx (numpy.ndarray): m-element vector where each entry contains the index of the nearest centroid for the corresponding observation, as determined by the k-means method.
+        k (int): Number of clusters.
+        nCL (int): Number of unique class labels.
+        p_parameter (float): The p-almost homogeneous parameter, a threshold for determining homogeneity.
+        h_threshold (int): Minimum number of observations required for a cluster to be considered in the homogeneity test.
+
+        Returns:
+        tuple: (rmCidx, rmXidx, DominantClassLabel, DominantClassLabel_Proportion, ObservationsPerCluster, nonDominantClassLabelNumber)
+            - p_almost_homogeneous_clusters (numpy.ndarray): Indices of clusters that satisfy the homogeneity criterion.
+            - rmXidx (numpy.ndarray): Indices of observations in clusters that satisfy the homogeneity criterion.
+            - DominantClassLabel (numpy.ndarray): k-element vector with the dominant class label for each cluster.
+            - DominantClassLabel_Proportion (numpy.ndarray): k-element vector with the proportion of the dominant class in each cluster.
+            - ObservationsPerCluster (numpy.ndarray): k-element vector with the number of observations in each cluster.
+            - nonDominantClassLabelNumber (numpy.ndarray): k-element vector with the number of observations that do not belong to the dominant class in each cluster.
+    """
+        # Step 0: Count the occurrences of each class in each cluster
+        count_matrix = np.zeros((k, nCL), dtype=int)  # Initialize a k x n matrix to count occurrences of each class in each cluster
+        np.add.at(count_matrix, (idx, y), 1)  # Count the occurrences of each class in each cluster
+
+        # Step 1: Calculate the dominant class label and its proportion for each cluster
+        DominantClassLabel = np.argmax(count_matrix, axis=1)                                # k-element vector. Dominant class label in each cluster
+        DominantClassLabel_Counts = count_matrix[np.arange(k), DominantClassLabel]          # k-element vector. Number of observations in each cluster that belong to the dominant class
+        ObservationsPerCluster = np.sum(count_matrix, axis=1)                               # k-element vector. Total number of observations in each cluster
+        DominantClassLabel_Proportion = DominantClassLabel_Counts / ObservationsPerCluster  # k-element vector. Proportion of the dominant class in each cluster
+
+        # Step 2: Calculate the number of observations that do not belong to the dominant class in each cluster
+        nonDominantClassLabelNumber = ObservationsPerCluster - DominantClassLabel_Counts
+
+        # Step 3: Determine which clusters satisfy the homogeneity criterion
+        rmCidx = np.where( (DominantClassLabel_Proportion >= (1 - p_parameter)) & (ObservationsPerCluster >= h_threshold) )[0]
+
+        # Step 4: Determine which observations are in clusters that satisfy the homogeneity criterion
+        #rmXidx = np.flatnonzero(np.isin(idx, rmCidx))
+        rmXidx = np.where(np.isin(idx, rmCidx))[0]
+
+        return rmCidx, rmXidx, DominantClassLabel, DominantClassLabel_Proportion, ObservationsPerCluster, nonDominantClassLabelNumber, count_matrix
+
+
+    def check_separability(self,X, y, idx, k:int, nCL:int, dm_threshold:float, return_full_dm:bool, dm_case:int=2):
+        """
+        Check the separability of clusters based on divergence measures.
+
+        Parameters:
+        X (numpy.ndarray): m x d matrix where each row contains the d-dimensional coordinates of an observation (m is the number of observations, d is the number of features).
+        y (numpy.ndarray): m-element vector where each entry contains the label of the corresponding observation. Labels are integers between 0 and nCL-1 (nCL is the number of unique class labels).
+        idx (numpy.ndarray): m-element vector where each entry contains the index of the nearest centroid for the corresponding observation, as determined by the k-means method.
+        k (int): Number of clusters.
+        nCL (int): Number of unique class labels.
+        dm_threshold (float): Threshold value for divergence measure.
+        return_full_dm (bool): If True, the full divergence measure matrix is calculated. Otherwise, return when the separability criterion is met.
+        dm_case (int, optional): Divergence measure case (default is 2).
+
+        Returns:
+        tuple: (criterion_met, mat_dm)
+            - criterion_met (bool): True if the separability criterion is met, False otherwise.
+            - mat_dm (numpy.ndarray): k x (nCL - 1) matrix of divergence measures.
+        """
+
+        criterion_met = False
+        mat_dm = np.zeros((k,nCL*(nCL-1)//2))
+
+        for c in range(k):
+            idx_c = np.where(idx == c)[0] # Indices of observations in cluster c
+            X_c = X[idx_c,:] # Observations in cluster c
+            y_c = y[idx_c] # Class labels of observations in cluster c
+            for a in range(0,nCL-1):
+                idx_c_a = np.where(y_c == a)[0] # Indices of observations in X_c with class a
+                Na = len(idx_c_a) # Number of observations in X_c with class a
+                if Na < 2: continue
+                X_c_a = X_c[idx_c_a,:]
+                for b in range(a+1,nCL):
+                    idx_c_b = np.where(y_c == b)[0] # Indices of observations in X_c with class b
+                    Nb = len(idx_c_b) # Number of observations in X_c with class b
+                    if Nb < 2: continue
+
+                    dmAlgorithm = DivergenceMeasureScipy(# Calculate divergence measure
+                        X_c_a,
+                        X_c[idx_c_b,:]
+                    )
+                    dm = dmAlgorithm.dm2()
+                    mat_dm[c,a*(nCL-1)-a*(a-1)//2+b-a-1] = dm
+                    if dm >= dm_threshold:
+                        criterion_met = True
+                        if not return_full_dm:
+                            return criterion_met, mat_dm
+        return criterion_met, mat_dm
 
 
     def initializeHistoryStructures(self):
@@ -35,7 +158,7 @@ class Fsp:
                 'C': [],
                 'dm_threshold': [],
                 'dm': [],
-                'ClassLabel_Proportion': [],
+                'count_matrix': [],
                 'DominantClassLabel': [],
                 'DominantClassLabel_Proportion': [],
                 'ClusterClassificationError': [],
@@ -46,37 +169,12 @@ class Fsp:
             }
         return rmH, H
 
-    def check_separability_criteria(self, k,nCL, dm_threshold, X, ClassLabel_Num, ClassLabel_Indices, opt):
-        mat_dm = np.zeros((k,nCL*(nCL-1)//2))
-        criterion_met = False
-        for c in range(k):
-            for a in range(0,nCL-1):
-                Na = ClassLabel_Num[c,a]
-                if Na < 2: continue
-                for b in range(a+1,nCL):
-                    Nb = ClassLabel_Num[c,b]
-                    if Nb < 2: continue
-                    dmAlgorithm = DivergenceMeasureScipy(
-                        X[ClassLabel_Indices[c,a], :],
-                        X[ClassLabel_Indices[c,b], :]
-                    )
-                    dm = dmAlgorithm.dm2()
-                    mat_dm[c,a*(nCL-1)-a*(a-1)//2+b-a-1] = dm
-                    if dm >= dm_threshold:
-                        criterion_met = True
-                        if not opt.return_full_dm:
-                            return criterion_met, mat_dm
-        return criterion_met, mat_dm
-
     def fit(self, X, y):
+        startTime = time.time()
+
+        #initializing data
         self.X = X
         self.y = y
-        return self.fit()
-
-    def fit(self):
-
-        startTime = time.time()
-        print(f'Tam dp X={self.X.size}, Tam do y={self.y.size}, Options={self.opt}')
 
         #Initializing history structures
         rmH, H = self.initializeHistoryStructures()
@@ -85,7 +183,7 @@ class Fsp:
         k = self.opt.initial_k       #Current value of k for k-means
         i = -1                   #iteration counter
         rmi = 0                 #The iteration counterfor when cluster removal occured
-        nCl = len(np.unique(self.y)) #Number of unique class labels
+        nCL = len(np.unique(self.y)) #Number of unique class labels
         N = self.X.shape[0]          #Number of observations
         p_parameter = self.opt.p_parameter #The p-almost homogeneous parameter
         dm_threshold = self.opt.dm_threshold #The divergence measure threshold
@@ -93,7 +191,6 @@ class Fsp:
 
 
         while self.X.shape[0] > 0:
-
             i += 1 #contador da iteracao
 
             if (self.opt.return_full_history):
@@ -106,69 +203,26 @@ class Fsp:
             idx = kmeans.labels_
             C = kmeans.cluster_centers_
 
-            #initializing structures to calculo de homogeneidade
-            Indices = np.zeros(k, dtype=object)
-            Indices_Num = np.zeros(k)
-
-            ClassLabel_Indices = np.zeros((k,nCl), dtype=object)
-            ClassLabel_Num = np.zeros((k,nCl))
-            ClassLabel_Proportion = np.zeros((k,nCl))
-
-            DominantClassLabel = np.zeros(k)
-            DominantClassLabel_Proportion = np.zeros(k)
-            nonDominantClassLabelNumber = np.zeros(k)
-
-            dm = np.array([])
-            rmXidx = np.array([])
-            sum1 = np.array([])
-
-            #cluster's evaluation
-            for c in range(k):
-                #Indices of the observations in X into cluster c
-                Indices[c] = np.where(idx == c)[0]
-                #Number of observations in X into cluster c
-                Indices_Num[c] = len(Indices[c])
-                #Class labels of the observations in X into cluster c
-                yc = self.y[Indices[c]]
-
-                #for each class label
-                for l in range(0,nCl):
-                    ClassLabel_Indices[c,l] = np.where(yc == l)[0]
-                    ClassLabel_Num[c,l] = len(ClassLabel_Indices[c,l])
-                    ClassLabel_Proportion[c,l] = ClassLabel_Num[c,l] / Indices_Num[c]
-
-                # Cluster classification error
-                # Let Xc and yc be the observations and class labels in cluster c
-                # N the oritional number of observations
-                # Nc the number of observations in cluster c
-
-                # Let lc be the dominant class label in cluster c
-                lc = np.argmax(ClassLabel_Proportion[c])
-                DominantClassLabel[c] = lc
-
-                # Let pc be the proportion of the dominant class label in cluste c
-                DominantClassLabel_Proportion[c] = ClassLabel_Proportion[c,lc]
-
-                # The cluster classification error is defined as:
-                # ClusterClassificationError(C) = sum(yc != lc) / Nc === mean(yc != lc)
-                nonDominantClassLabelNumber[c] = np.sum(yc != c) # Contando os resultados true da verificacao
+            #Verificar criterio de homogeneidade
+            (rmCidx,
+            rmXidx,
+            DominantClassLabel,
+            DominantClassLabel_Proportion,
+            ObservationsPerCluster,
+            nonDominantClassLabelNumber,
+            count_matrix) = self.check_homogeneity(self.y, idx, k, nCL, self.opt.p_parameter, self.opt.h_threshold)
 
             #Calcular do erro de cada cluster (local)
-            ClusterClassificationError = np.divide(nonDominantClassLabelNumber, Indices_Num)
+            ClusterClassificationError = np.divide(nonDominantClassLabelNumber, ObservationsPerCluster)
 
             # Calculate the global Classification error
             ClassificationError = sum2 + sum(nonDominantClassLabelNumber) / N
 
-            #Verificar criterio de homogeneidade
-            vec_max = np.max(ClassLabel_Proportion,1)
-            vec = (vec_max >= (1 - self.opt.p_parameter)) & (Indices_Num > self.opt.h_threshold)
-
             # Se existe cluste p-quase homogeneo
-            if np.sum(vec) > 0:
-                rmCidx = np.where(vec)[0]
-                rmXidx = np.concatenate( Indices[rmCidx] )
+            sum1 = np.array([])
+            dm = np.array([])
+            if len(rmCidx) > 0:
                 rmC = C[rmCidx, :]
-                # rmXidx = rmXidx.astype(int) O Gabriel fez isso, não sabemos o porque
 
                 rmNonDominantClassLabelNumberbyN = nonDominantClassLabelNumber[rmCidx] / N
                 sum1 = sum(rmNonDominantClassLabelNumberbyN)
@@ -181,13 +235,10 @@ class Fsp:
                 rmH['rmC'].append(rmC)
                 rmH['dm_threshold'].append(dm_threshold)
                 rmH['dm'].append(dm)
-                rmH['rmDominantClassLabel'].append(DominantClassLabel[vec])
-                rmH['rmDominantClassLabel_Proportion'].append(DominantClassLabel_Proportion[vec])
+                rmH['rmDominantClassLabel'].append(DominantClassLabel[rmCidx])
+                rmH['rmDominantClassLabel_Proportion'].append(DominantClassLabel_Proportion[rmCidx])
                 rmH['rmXidx'].append(rmXidx)
                 rmH['sum1'].append(sum1)
-
-                # rmH_list.append(rmH.copy())  O Gabriel fez isso, não sabemos o porque
-                # rmH, _ = initializeStructures(opt)  O Gabriel fez isso, não sabemos o porque
 
                 self.X = np.delete(self.X, rmXidx, axis=0)
                 self.y = np.delete(self.y, rmXidx, axis=0)
@@ -199,8 +250,7 @@ class Fsp:
                     dm_threshold = dm_threshold_previous / np.sqrt(ClassificationError / ClassificationError_previous)
 
                 #Calculating the divergence measure
-
-                criterion_met, dm = self.check_separability_criteria(k,nCl, dm_threshold, self.X, ClassLabel_Num, ClassLabel_Indices, self.opt)
+                criterion_met, dm = self.check_separability(self.X, self.y, idx, k, nCL, dm_threshold, self.opt.return_full_dm, self.opt.dm_case);
 
                 # YES, there is separable region. Set k = k + 1.
                 if criterion_met:
@@ -238,12 +288,11 @@ class Fsp:
             if self.opt.return_full_history:
                 H['dm_threshold'].append(dm_threshold)
                 H['dm'].append(dm)
-                H['ClassLabel_Proportion'].append(ClassLabel_Proportion)
+                H['count_matrix'].append(count_matrix)
                 H['DominantClassLabel'].append(DominantClassLabel)
                 H['DominantClassLabel_Proportion'].append(DominantClassLabel_Proportion)
                 H['ClusterClassificationError'].append(ClusterClassificationError)
                 H['ClassificationError'].append(ClassificationError)
-                H['ClassLabel_Proportion'].append(ClassLabel_Proportion)
                 H['sum1'].append(sum1)
                 H['C'].append(C)
                 H['rmXidx'].append(rmXidx)
@@ -259,5 +308,6 @@ class Fsp:
         result = {'rmH':rmH,'H':H,'opt':self.opt,'InSampleError':np.sum(rmH['sum1']),'runtime':time.time() - startTime}
         return result
 
-        def helloFsp(self):
-            return "hello fsp"
+
+    def helloFsp(self):
+        return "hello fsp"
--- test/test_fsp.py
+++ test/test_fsp.py
@@ -1,24 +1,65 @@
 from sklearn.datasets import load_iris
 from fsp.fsp import Fsp
+from fsp.options import Options
+from sklearn.cluster import KMeans
+from sklearn.model_selection import train_test_split
+from numpy.random import RandomState
+import numpy as np
 import pandas as pd
-
 
 def preProccessIrisDataset():
     # Load the iris dataset
     iris_data = load_iris()
-    return iris_data.data[50:,[2,3]], iris_data.target[50:] - 1 #converting classes to 0 based aprouch
+    #return iris_data.data[50:,[2,3]], iris_data.target[50:] - 1 #converting classes to 0 based aprouch
+    return iris_data.data, iris_data.target
+
 
 def test_fsp():
+
+    # Define as configurações globais do pandas
+    pd.set_option('display.max_colwidth', None)
+    pd.set_option('display.width', None)
+    pd.set_option('display.expand_frame_repr', False)
 
     #instancia o um dataset
     X, y = preProccessIrisDataset()
 
-    #instancia o algoritmo
-    fsp = Fsp(X,y)
+    #prepara os datasets de treino e teste
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=20, random_state=42)
+
+    #instancia o fsp
+    fsp = Fsp(opt = Options(return_full_history=True,dm_threshold=0.1,p_parameter=0.01,kmeans_random_state=42))
 
     #treina a o fsp
-    result = fsp.fit()
+    model = fsp.fit(X_train,y_train)
 
-    #Exibe em formato pandas
-    T = pd.DataFrame(result['rmH'])
-    print(T)
+    print("\nrmH\n",pd.DataFrame(model['rmH']))
+    print('\nHistorico H\n',pd.DataFrame(model['H']))
+
+    #testa o fsp
+    y_pred, y_pred_Proportion = fsp.predict_case1(X_test,model['rmH'],X_train,y_train)
+    print("y_test =",y_test)
+    print("y_pred.shape",y_pred.shape)
+    print("\ny_pred\n",y_pred)
+    print("\ny_pred_Proportion.shape",y_pred_Proportion.shape)
+    print("\ny_pred_Proportion\n",y_pred_Proportion)
+
+    erro = np.mean(y_test != y_pred)
+    print("Erro de classificação:", erro)
+
+def test_kmeans():
+
+    #instancia o um dataset
+    X, y = preProccessIrisDataset()
+
+    #print(X)
+    #print(y)
+    rs = RandomState(42)
+
+    kmeans = KMeans(n_clusters=4,max_iter= 1000, random_state=rs,  n_init=1).fit(X)
+    idx = kmeans.labels_
+    C = kmeans.cluster_centers_
+
+    print(idx)
+    print(C)
+
