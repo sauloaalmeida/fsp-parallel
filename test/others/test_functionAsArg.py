8dfe672f74b8687562232ad3be38e7bfbda291c4
Saulo Andrade <sauloandrade@gmail.com>
2024-07-02 13:59:38 -0300

    renaming some test files

---
+++ test/others/test_functionAsArg.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+import types
+
+@dataclass
+class Testando:
+    texto: str = ""
+    funcao: types.MethodType = lambda texto : texto
+
+    def imprime(self):
+        print(self.funcao(self.texto))
+
+def emCaixaAlta(texto):
+    return texto.upper()
+
+def emCaixaBaixa(texto):
+    return texto.lower()
+
+def reverso(texto):
+    return texto[::-1]
+
+def main():
+    test = Testando("Texto de Teste 1")
+    test.imprime()
+
+    test2 = Testando("Texto de Teste 2", funcao = emCaixaBaixa)
+    test2.imprime()
+
+    test2.funcao = emCaixaAlta
+    test2.imprime()
+
+    test2.funcao = reverso
+    test2.imprime()
+
+main()
