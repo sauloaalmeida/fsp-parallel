5f4f12857ae9617a01a9fb1c3904eeb70bfde911
Saulo Andrade <sauloandrade@gmail.com>
2024-08-06 10:38:57 -0300

    last hyperparam version updated

--- src/fsp/evaluate/hyperparam_optimization.py
+++ src/fsp/evaluate/hyperparam_optimization.py
@@ -30,11 +30,14 @@ def fsp_HyperparameterTuning_skopt(dataset_name):
     accuracy (float): The accuracy of the model on the test set.
     OptimizeResult: The result of the optimization process.
     """
-    absPath = Path(".").cwd() / "test" / "benchmark" / "fsp" / "Datasets" / f"{dataset_name}.csv"
+    projectRootAbsPath = Path(".").cwd()
+    datasetAbsDirPath = projectRootAbsPath / "test" / "benchmark" / "fsp" / "Datasets"
+    resultsAbsDirPath = projectRootAbsPath / "test" / "benchmark" / "fsp" / "Results"
+    datasetAbsPath = datasetAbsDirPath / f"{dataset_name}.csv"
 
     # Load [X,y] matrix based on the dataset_name type (str or numpy.ndarray)
     if isinstance(dataset_name, str):
-        X_y = pd.read_csv(absPath,header=None).values
+        X_y = pd.read_csv(datasetAbsPath,header=None).values
     elif isinstance(dataset_name, np.ndarray):
         X_y = dataset_name
         dataset_name = None
@@ -44,15 +47,6 @@ def fsp_HyperparameterTuning_skopt(dataset_name):
     # Adjust class labels if necessary
     if np.min(np.unique(X_y[:, -1])) == 1:
         X_y[:, -1] -= 1
-
-    # Split dataset into features (X) and target (y)
-    X, y = X_y[:, :-1], X_y[:, -1].astype(int)
-
-    # Split the dataset into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
-
-    # Combine X_train and y_train for cross-validation
-    X_y_train = np.hstack((X_train, y_train.reshape(-1, 1)))
 
     # Define the objective function for hyperparameter optimization
     def objective(params):
@@ -70,14 +64,14 @@ def fsp_HyperparameterTuning_skopt(dataset_name):
         )
         # Run evaluation in parallel and collect results in single_run
         single_run = Parallel(n_jobs=-1)(
-            delayed(FSP_cvLoss)(X_y_train, opt, predict_method=int(predict_method), cv=LeaveOneOut())
+            delayed(FSP_cvLoss)(X_y, opt, predict_method=int(predict_method), cv=LeaveOneOut())
             for _ in range(10)
         )
         # Return the objective value for optimization
         return np.mean(single_run) + 2 * np.std(single_run)
 
     # Define the space of hyperparameters to search
-    kmax = int( 10 + (90/9900)*(max(100,X.shape[0])-100) )
+    kmax = int( 10 + (90/9900)*(max(100,X_y.shape[0])-100) )
     print(f"\nintial_k space search = {[1,kmax]}\n")
     space = [
         Integer(0, 1,  name='Standardize'),
@@ -93,14 +87,16 @@ def fsp_HyperparameterTuning_skopt(dataset_name):
 
     # Define initial parameter combinations
     # Case 1: If the file "Results/{dataset_name}_fsp_evaluating_accuracy_KFold0.npz" exist
-    if os.path.isfile(f"Results/{dataset_name}_fsp_evaluating_accuracy_KFold0.npz"):
-        loaded_data = np.load(f"Results/{dataset_name}_fsp_evaluating_accuracy_KFold0.npz", allow_pickle=True)
+    if os.path.isfile(resultsAbsDirPath / f"{dataset_name}_fsp_evaluating_accuracy_KFold0.npz"):
+        loaded_data = np.load(resultsAbsDirPath / f"{dataset_name}_fsp_evaluating_accuracy_KFold0.npz", allow_pickle=True)
         dataset_results = loaded_data['dataset_results'].tolist()
         x0 = []; y0 = []
         for R in dataset_results:
             opt = R['opt']
+            # Add two initial points for each previous result (one for each predict_method)
             x0.append([opt.Standardize, opt.initial_k, opt.p_parameter, opt.h_threshold, opt.dm_case, opt.s_parameter, opt.dm_threshold, opt.update_s_parameter, 1])
             x0.append([opt.Standardize, opt.initial_k, opt.p_parameter, opt.h_threshold, opt.dm_case, opt.s_parameter, opt.dm_threshold, opt.update_s_parameter, 2])
+            # Calculate and store the corresponding y values (1 - accuracy + 2*std)
             y0.extend([1-R['MeanStd_accuracies_predict1'][0]/100+2*R['MeanStd_accuracies_predict1'][1]/100, 1-R['MeanStd_accuracies_predict2'][0]/100+2*R['MeanStd_accuracies_predict2'][1]/100])
     # Case 2: If the file "Results/{dataset_name}_fsp_evaluating_accuracy_KFold0.npz" doesn't exist
     else:
@@ -135,31 +131,31 @@ def fsp_HyperparameterTuning_skopt(dataset_name):
     # Perform the optimization
     print(f"x0={x0}")
     print(f"y0={y0}\n")
-    # len_x0 = len(x0) if y0 is None else 0
-    len_x0 = 0
-    # n_initial_points = 10
-    # n_calls = 10
-    n_initial_points = 2
-    n_calls = 2
+    len_x0 = len(x0) if y0 is None else 0
+    n_initial_points = 10
+    n_calls = 100
+    # len_x0 = 0
+    # n_initial_points = 2
+    # n_calls = 2
     OptimizeResult = gp_minimize(
         func=objective,
         dimensions=space,
-        # x0=x0,
-        # y0=y0,
+        x0=x0,
+        y0=y0,
         n_initial_points=n_initial_points,
         n_calls=len_x0 + n_initial_points + n_calls,
         n_jobs=-1,
         verbose=True
     )
 
-    # Plot the convergence history
+    # Plot the convergence history to visualize optimization progress
     plot_convergence(OptimizeResult)
 
     # Print the function value at the minimum
     max_best_value = 100*(1 - OptimizeResult.fun)
     print("\nMinimum function value:", OptimizeResult.fun)
 
-    # Retrieve the best parameters from the optimizer
+    # Retrieve and print the best parameters from the optimizer
     best_params = OptimizeResult.x
     print("\nBest parameters:", best_params)
 
@@ -180,49 +176,29 @@ def fsp_HyperparameterTuning_skopt(dataset_name):
     print("\nOptimized options:")
     print(opt)
 
-    # Avaliate the model with the optimized options
-    vecAcc = np.zeros(10)
-    # for i in range(10):
-    #     # Train the model with the optimized options
-    #     mdl = fsp(X_train, y_train, opt)
-    #     # Predict the test set using the trained model
-    #     ypred, _ = fsp_predict(mdl, X_test, predict_method)
-    #     # Calculate the accuracy of the model
-    #     vecAcc[i] = 100*np.mean(y_test == ypred)
-    # Train and evaluate the model in parallel using a lambda function
-    vecAcc = Parallel(n_jobs=-1)(
-        delayed(lambda i: 100 * np.mean(
-            y_test == fsp_predict(fsp(X_train, y_train, opt), X_test, predict_method)[0]
-        ))(i) for i in range(10)
-    )
-
-    # Calculate and print the accuracy of the model
-    MeanStd_accuracies = [np.mean(vecAcc), np.std(vecAcc)]
-    print(f'\nMeanStd_accuracies: {MeanStd_accuracies}')
-
     # Save results in the "Results" folder if the input "dataset_name" is of type str
     if isinstance(dataset_name, str):
         # Create the "Results" folder if it doesn't exist
-        results_dir = "Results"
-        if not os.path.exists(results_dir):
-            os.makedirs(results_dir)
+        #results_dir = "Results"
+        if not os.path.exists(resultsAbsDirPath):
+            os.makedirs(resultsAbsDirPath)
 
         # Define the result file name
-        result_file = os.path.join(results_dir, f"{dataset_name}_fsp_HyperparameterTuning.csv")
+        result_file = os.path.join(resultsAbsDirPath, f"{dataset_name}_fsp_HyperparameterTuning.csv")
 
         # Check if the file already exists
         file_exists = os.path.isfile(result_file)
 
-        # Save opt, predict_method, accuracy, and OptimizeResult in a CSV file
+        # Save opt, predict_method, and max_best_value in a CSV file
         with open(result_file, mode='a', newline='') as f:
             writer = csv.writer(f)
 
             # Write header only if the file doesn't already exist
             if not file_exists:
-                writer.writerow(['optimizer', 'opt', 'predict_method', 'max_best_value', 'MeanStd_accuracies'])
+                writer.writerow(['optimizer', 'opt', 'predict_method', 'max_best_value'])
 
             # Write the results
-            writer.writerow(["skopt", opt, predict_method, max_best_value, MeanStd_accuracies])
+            writer.writerow(["skopt", opt, predict_method, max_best_value])
 
     # Return results
-    return opt, predict_method, max_best_value, MeanStd_accuracies, OptimizeResult
+    return opt, predict_method, max_best_value, OptimizeResult
