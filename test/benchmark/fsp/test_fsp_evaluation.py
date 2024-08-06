922db0001bb6a1f2c6d22bca3ca89a2d31a753fb
Saulo Andrade <sauloandrade@gmail.com>
2024-08-06 13:11:38 -0300

    Adding test to verify torch cpu and gpu execution

--- test/benchmark/fsp/test_fsp_evaluation.py
+++ test/benchmark/fsp/test_fsp_evaluation.py
@@ -3,6 +3,7 @@ import time
 from pathlib import Path
 from fsp.options import Options
 from fsp.evaluate.hyperparam_optimization import fsp_HyperparameterTuning_skopt
+from fsp.evaluate.fspaccuracy import fsp_evaluating_accuracy
 import joblib
 
 def testVersionJoblib():
@@ -10,3 +11,6 @@ def testVersionJoblib():
 
 def test_hyperparam_optimization():
     fsp_HyperparameterTuning_skopt("Iris")
+
+def test_fsp_evaluating_accuracy():
+    fsp_evaluating_accuracy(dataset_name="Iris", opt=[Options(),Options(distance_method=3),Options(distance_method=4)], KFold=0, NumberOfRuns=10)
