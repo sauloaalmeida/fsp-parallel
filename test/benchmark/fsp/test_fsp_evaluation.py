1c190ed639639cd2581c71844d28e5109d04ca1f
Saulo Andrade <sauloandrade@gmail.com>
2024-09-03 13:05:55 -0300

    fixing test execution parameters

--- test/benchmark/fsp/test_fsp_evaluation.py
+++ test/benchmark/fsp/test_fsp_evaluation.py
@@ -19,8 +19,8 @@ def test_benchmark1_fsp_evaluating_accuracy():
     fsp_evaluating_accuracy(dataset_name=None, opt=Options.list1(), KFold=0, NumberOfRuns=10)
 
 def test_benchmark2_fsp_evaluating_accuracy():
-    opt1 = Options(Standardize=True, initial_k=50, h_threshold=4, dm_case=1, s_parameter=0.15, distance_method=1)
-    opt2 = Options(Standardize=True, initial_k=50, h_threshold=4, dm_case=1, s_parameter=0.15, distance_method=3)
-    opt3 = Options(Standardize=True, initial_k=50, h_threshold=4, dm_case=1, s_parameter=0.15, distance_method=4)
+    opt1 = Options(Standardize=True, initial_k=45, p_parameter=0.01, h_threshold=4, dm_case=1, dm_threshold=3, update_s_parameter=True, s_parameter=0.15, distance_method=1)
+    opt2 = Options(Standardize=True, initial_k=45, p_parameter=0.01, h_threshold=4, dm_case=1, dm_threshold=3, update_s_parameter=True, s_parameter=0.15, distance_method=3)
+    opt3 = Options(Standardize=True, initial_k=45, p_parameter=0.01, h_threshold=4, dm_case=1, dm_threshold=3, update_s_parameter=True, s_parameter=0.15, distance_method=4)
 
     fsp_evaluating_accuracy(dataset_name=["ElectricalFaultDetection"], opt=[opt1, opt2, opt3], KFold=0, NumberOfRuns=10)
